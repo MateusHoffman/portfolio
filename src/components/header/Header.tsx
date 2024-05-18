791b2d353e5ca0bcb5007aadf177395d5ea9795a
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-18 15:33:15 -0300

    Create component Header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-white px-10 border-b-[1px] border-b-[#8C8C8C] border-opacity-20 min-h-12 w-full"></header>
+  );
+}
