8b91c4f8986b6098d2bfa8e8b0f53c16f7038dc4
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-18 16:22:44 -0300

    Add logo in Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-white px-10 border-b-[1px] border-b-[#8C8C8C] border-opacity-20 min-h-12 w-full"></header>
+    <header className="bg-white md:px-0 px-6 border-b-[1px] border-b-[#8C8C8C] border-opacity-20 h-12 w-full flex items-center justify-center">
+      {/* eslint-disable @next/next/no-img-element */}
+      <div className="md:w-3/5 2xl:w-5/12 w-full">
+      <img className="h-9" src="logo_hoffman.png" alt="Logo Hoffman" />
+      </div>
+    </header>
   );
 }
