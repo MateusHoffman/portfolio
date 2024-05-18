117ddb11edf18597f32760fb6e5313839ee4ab10
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-18 17:23:55 -0300

    Add section Experience in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Header from "@/components/header/Header";
 import Profile from "./_components/profile/Profile";
 import About from "./_components/about/About";
+import Experience from "./_components/experience/Experience";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-screen bg-light max-w-full">
       <Header />
-      <main className="flex md:pt-3 items-center gap-2 flex-col">
+      <main className="flex md:pt-3 items-center gap-2 flex-col pb-6">
         <Profile />
         <About />
+        <Experience />
       </main>
     </div>
   );
