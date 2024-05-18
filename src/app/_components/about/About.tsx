90ec44ccd92b2389312dae84470c118619216469
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-18 16:53:53 -0300

    Create section About

---
+++ src/app/_components/about/About.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+
+export default function About() {
+
+  function handleSeeMore() {
+    console.log('Abrir modal para ver o conteúdo completo da seção About');
+  }
+
+  return (
+    <section className="md:w-3/5 2xl:w-5/12 md:p-6 p-4 w-full border-[1px] border-[#8C8C8C] border-opacity-20 md:rounded-xl bg-white">
+      <h1 className="text-black text-opacity-90 text-xl font-semibold leading-5">
+        Sobre
+      </h1>
+      <p className="py-3 text-black text-opacity-90 text-sm leading-5 font-normal">
+        Passei na Universidade Federal do Rio de Janeiro em Ciências Contábeis,
+        mas tranquei, pois encontrei o que realmente quero para minha vida.
+      </p>
+      <div className="w-full flex justify-end">
+        <button onClick={handleSeeMore}>
+          <span className="text-black text-opacity-60 text-sm leading-5 font-normal">Ver mais...</span>
+        </button>
+      </div>
+    </section>
+  );
+}
