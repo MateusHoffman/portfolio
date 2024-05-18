a745f0a18ec7f9be17712dd6f0a903bc5dee0fed
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-18 16:54:02 -0300

    Add section About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header/Header";
 import Profile from "./_components/profile/Profile";
+import About from "./_components/about/About";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main className="flex md:pt-3 items-center gap-3 flex-col">
         <Profile />
+        <About />
       </main>
     </div>
   );
