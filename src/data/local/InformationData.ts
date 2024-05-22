dfd8329e1b2f9fddb4403510338ebcdda226c7fa
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-22 00:28:21 -0300

    Create data information

---
+++ src/data/local/InformationData.ts
@@ -0,0 +1,24 @@
+export const informationData = {
+  information: [
+    {
+      icon: 'linkedin.png',
+      name: 'Email',
+      value: 'mateushoffmandev@gmail.com',
+    },
+    {
+      icon: 'linkedin.png',
+      name: 'Número de celular',
+      value: '+55 19 98942-8951',
+    },
+    {
+      icon: 'linkedin.png',
+      name: 'Endereço',
+      value: 'Campinas, São Paulo, Brasil',
+    },
+    {
+      icon: 'linkedin.png',
+      name: 'Idade',
+      value: '21 anos',
+    },
+  ]
+}
