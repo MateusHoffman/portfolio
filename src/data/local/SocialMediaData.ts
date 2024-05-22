e2a6c942187249adae9056240f2ec51f0ff5bc0d
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-22 00:28:26 -0300

    Create data social media

---
+++ src/data/local/SocialMediaData.ts
@@ -0,0 +1,22 @@
+export const socialMediaData = {
+  socialMedia: [
+    {
+      logo: 'linkedin.png',
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/mateushoffman/',
+      showLink: 'linkedin.com/in/mateushoffman',
+    },
+    {
+      logo: 'github.png',
+      name: 'GitHub',
+      link: 'https://github.com/MateusHoffman',
+      showLink: 'github.com/MateusHoffman',
+    },
+    {
+      logo: 'instagram.png',
+      name: 'Instagram',
+      link: 'https://www.instagram.com/maahoffman/',
+      showLink: 'instagram.com/maahoffman/',
+    },
+  ]
+}
