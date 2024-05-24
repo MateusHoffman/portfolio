4750c6627a1944f81d2870c7e98b42627caa7e2a
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-24 14:36:06 -0300

    Fix position modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -23,7 +23,7 @@ export default function Modal(props: IProps) {
     >
       {/* eslint-disable @next/next/no-img-element */}
       <div className="fixed inset-0 flex justify-center items-center bg-[#00000080] transition-opacity duration-300">
-        <DialogPanel className="w-[95%] md:w-3/5 2xl:w-5/12 max-h-[95%] bg-white rounded-xl flex flex-col">
+        <DialogPanel className="w-[95%] max-h-[95%] 2xl:w-5/12 bg-white rounded-xl flex flex-col">
           <header>
             <div className="flex justify-between items-center pr-4">
               <h1 className="py-4 px-6 font-semibold text-xl text-[#000000e6] w-full">
