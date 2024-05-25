118de397398bc0c7f329e65702dfaacbc3742b8a
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-25 15:45:25 -0300

    Remove dark mode input

--- src/app/_components/profile/_components/ModalSendMessage.tsx
+++ src/app/_components/profile/_components/ModalSendMessage.tsx
@@ -59,7 +59,7 @@ export default function ModalSendMessage(props: IProps) {
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-[#00000080] text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border border-[#00000080] text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
               placeholder="email@gmail.com"
               required
               onChange={(e) => setEmail(e.target.value)}
@@ -75,7 +75,7 @@ export default function ModalSendMessage(props: IProps) {
             <input
               type="text"
               id="subject"
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-[#00000080] shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-[#00000080] shadow-sm focus:ring-primary-500 focus:border-primary-500"
               placeholder="Descreva o assunto"
               required
               onChange={(e) => setSubject(e.target.value)}
@@ -93,13 +93,13 @@ export default function ModalSendMessage(props: IProps) {
               rows={6}
               required
               onChange={(e) => setMessage(e.target.value)}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-[#00000080] focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-[#00000080] focus:ring-primary-500 focus:border-primary-500"
               placeholder="Deixe sua mensagem..."
             ></textarea>
           </div>
           <button
             type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
             disabled={loading}
           >
             {loading ? "Enviando..." : "Enviar mensagem"}
