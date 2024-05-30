b3785d52acf50591daa3cb90658a671a106c1701
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 12:06:41 -0300

    Added footer in section Experience

--- src/app/_components/experience/Experience.tsx
+++ src/app/_components/experience/Experience.tsx
@@ -13,22 +13,24 @@ export default function Experience() {
 
   return (
     <>
-      <section className="md:w-3/5 2xl:w-5/12 md:p-6 p-4 w-full border-[1px] border-[#8C8C8C] border-opacity-20 md:rounded-xl bg-white">
+      <section className="md:w-3/5 2xl:w-5/12 w-full border-[1px] border-[#8C8C8C] border-opacity-20 md:rounded-xl bg-white">
         {/* eslint-disable @next/next/no-img-element */}
-        {/* Título de seção */}
-        <h1 className="text-black text-opacity-90 text-xl font-semibold leading-5">
-          Experiência
-        </h1>
-        {/* Lista de experiências */}
-        <ListExperience handleSeeMore={handleSeeMore} />
-        {/* Botão "Ver mais..." */}
-        <div className="w-full flex justify-end">
-          <button onClick={handleSeeMore}>
-            <span className="text-black text-opacity-60 text-base leading-5 font-normal">
-              Ver mais...
+        <header className="px-4 pt-4 md:px-6 md:pt-6">
+          <h1 className="text-black text-opacity-90 text-xl font-semibold leading-5">
+            Experiência
+          </h1>
+        </header>
+        <main className="px-4 md:px-6">
+          <ListExperience handleSeeMore={handleSeeMore} />
+        </main>
+
+        <footer className="px-4 md:px-6 py-2 border-t-[1px] border-t-[#8C8C8C] border-opacity-20 hover:bg-[#8c8c8c1a]">
+          <button onClick={handleSeeMore} className="w-full text-center">
+            <span className="text-base text-[#000000BF] font-semibold">
+              Ver tudo...
             </span>
           </button>
-        </div>
+        </footer>
       </section>
       <ModalExperience
         isOpen={isOpenModalExperience}
