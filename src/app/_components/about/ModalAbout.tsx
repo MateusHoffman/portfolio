a9b68a371164480ad6d96710a3cfaa37d5d1c1fc
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 19:50:42 -0300

    Implemented function formatTextWithLineBreaks

--- src/app/_components/about/ModalAbout.tsx
+++ src/app/_components/about/ModalAbout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Modal from "@/components/modal/Modal";
 import { aboutData } from "@/data/local/AboutData";
-import { boldWordsInText } from "@/app/util/format";
+import { formatTextWithLineBreaks } from "@/app/util/format";
 
 interface IProps {
   isOpen: boolean;
@@ -9,32 +9,27 @@ interface IProps {
 }
 
 export default function ModalAbout(props: IProps) {
-  const wordsToBold = [
-    "Front-End",
-    "mobile",
-    "web",
-    "dois anos",
-    "soft skills",
-    "JavaScript",
-    "TypeScript",
-    "Kotlin",
-    "React",
-    "Native",
-    "Jetpack Compose",
-    "responsividade",
-    "testes",
-    "desempenho",
-  ];
+  // const wordsToBold = [
+  //   "Front-End",
+  //   "mobile",
+  //   "web",
+  //   "dois anos",
+  //   "soft skills",
+  //   "JavaScript",
+  //   "TypeScript",
+  //   "Kotlin",
+  //   "React",
+  //   "Native",
+  //   "Jetpack Compose",
+  //   "responsividade",
+  //   "testes",
+  //   "desempenho",
+  // ];
 
   return (
     <Modal isOpen={props.isOpen} setIsOpen={props.setIsOpen} title="Sobre">
       <div>
-        <p
-          className="py-3 text-black text-opacity-90 text-base leading-5 font-normal"
-          dangerouslySetInnerHTML={{
-            __html: boldWordsInText(aboutData.mainText, wordsToBold),
-          }}
-        />
+        {formatTextWithLineBreaks(aboutData.mainText + aboutData.completeText)}
       </div>
     </Modal>
   );
