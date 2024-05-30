1e58b6bc8c51343fe552d5331dc36ed1c91b19f4
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-29 23:53:46 -0300

    Updated page Resume, add iframe to mobile

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { isMobile } from "react-device-detect";
 
 export default function Resume() {
   return (
     <div className="h-screen w-screen overflow-hidden bg-gray-800">
-      <embed
-        src='/mateus_hoffman_curriculo.pdf'
-        type="application/pdf"
-        className="absolute top-0 left-0 w-full h-full"
-      />
+      {isMobile ? (
+        <iframe
+          src="/mateus_hoffman_curriculo.pdf"
+          title="Resume"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      ) : (
+        <embed
+          src="/mateus_hoffman_curriculo.pdf"
+          type="application/pdf"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      )}
     </div>
   );
 }
