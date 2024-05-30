2f2a7653805a21ad6b464406812809392fe9b1a7
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 12:03:26 -0300

    Updated number line and implemented a function cleanURL

--- src/app/_components/recommendation/_components/ListRecommendations.tsx
+++ src/app/_components/recommendation/_components/ListRecommendations.tsx
@@ -1,3 +1,4 @@
+import { cleanURL } from "@/app/util/format";
 import { recommendationData } from "@/data/local/RecommendationData";
 import React from "react";
 
@@ -19,24 +20,24 @@ export default function ListRecommendations() {
             </div>
             <div className="w-full">
               <div className="flex flex-col">
-                <span className="text-[#000006] text-base font-semibold leading-5">
+                <span className="text-[#000006] text-base font-semibold leading-5 line-clamp-1">
                   {recommendation.name}
                 </span>
-                <span className="text-[#000006] text-base font-normal leading-5">
+                <span className="text-[#000006] text-base font-normal leading-5 line-clamp-1">
                   {recommendation.description}
                 </span>
                 <a
                   href={recommendation.link}
-                  className="text-[#0a66c2] text-base font-normal leading-5"
+                  className="text-[#0a66c2] text-base font-normal leading-5 line-clamp-1"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  {recommendation.link}
+                  {cleanURL(recommendation.link)}
                 </a>
               </div>
               <div className="pt-3">
                 <span
-                  className={`text-base text-[#000000e6] leading-5 font-normal`}
+                  className={`text-base text-[#000000e6] leading-5 font-normal line-clamp-5`}
                   style={{ whiteSpace: "pre-line" }}
                 >
                   {recommendation.message}
