1c4aa3f14a8f056cb87fb64b99da037ff1f8b39a
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 00:11:12 -0300

    Remove lib react-device-detect

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { isMobile } from "react-device-detect";
 
 export default function Resume() {
   return (
     <div className="h-screen w-screen overflow-hidden bg-gray-800">
-      {isMobile ? (
-        <iframe
-          src="/mateus_hoffman_curriculo.pdf"
-          title="Resume"
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      ) : (
-        <embed
-          src="/mateus_hoffman_curriculo.pdf"
-          type="application/pdf"
-          className="absolute top-0 left-0 w-full h-full"
-        />
-      )}
+      <embed
+        src="/mateus_hoffman_curriculo.pdf"
+        type="application/pdf"
+        className="absolute top-0 left-0 w-full h-full"
+      />
     </div>
   );
 }
