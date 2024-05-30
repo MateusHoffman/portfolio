f0cd5b8e4cab5b83063946e867ad47ee0b9c2fc0
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 11:28:39 -0300

    Created ModalRecommendation

---
+++ src/app/_components/recommendation/ModalRecommendation.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Modal from "@/components/modal/Modal";
+import ListRecommendations from "./_components/ListRecommendations";
+
+interface IProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+export default function ModalRecommendation({
+  isOpen,
+  setIsOpen,
+}: IProps) {
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Recomendações">
+      <ListRecommendations />
+    </Modal>
+  );
+}
