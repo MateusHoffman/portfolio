921d92d4a6fd7285243de4e383ca0b4e4a70862e
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 10:16:27 -0300

    Updated description ProfileData

--- src/data/local/ProfileData.ts
+++ src/data/local/ProfileData.ts
@@ -2,7 +2,7 @@ export const profileData = {
   name: "Mateus Hoffman",
   picture: "https://media.licdn.com/dms/image/C4D03AQFz4U-QOZT7nQ/profile-displayphoto-shrink_200_200/0/1662759513105?e=1721260800&v=beta&t=zL2kHEAVJSa7sTVYjSqoEqoOyijzQkK9dnd1bofwRhg",
   banner: "/banner.png",
-  description: "Desenvolvedor Mobile | React Native, TypeScript, Git, JavaScript",
+  description: "Front-End Mobile | TypeScript, JavaScript, React Native e Git",
   city: "Campinas",
   state: "São Paulo",
   country: "Brasil"
