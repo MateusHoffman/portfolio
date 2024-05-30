ec5a8035103e7118974d7b5c83e4e208ed51f8ae
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 11:28:24 -0300

    Added divider between experiences

--- src/app/_components/experience/_components/ListExperience.tsx
+++ src/app/_components/experience/_components/ListExperience.tsx
@@ -16,69 +16,74 @@ export default function ListExperience({
   modal = false,
 }: IProps) {
   return (
-    <div>
-      {experienceData.experiences.map((experience) => (
-        <div className="py-3 flex flex-row" key={experience.id}>
-          <div className="flex flex-col pr-3">
-            <img
-              className="w-12 aspect-square"
-              src={experience.imgLogo}
-              alt="Logo empresa"
-            />
-          </div>
-          <div className="w-full">
-            <div className="flex flex-col">
-              <span className="text-[#000006] text-base font-semibold leading-5">
-                {experience.company}
-              </span>
-              <span className="text-[#000006] text-base font-normal leading-5">
-                {experience.jobType} ·{" "}
-                {formatDateRange(experience.startDate, experience.endDate)}
-              </span>
-              <span className="text-[#00000099] text-base font-normal leading-5">
-                {formatTxtLocation(experience)}
-              </span>
+    <>
+      {experienceData.experiences.map((experience, index) => (
+        <div key={experience.id}>
+          {index !== 0 && (
+            <div className="h-px w-full bg-[#8C8C8C] bg-opacity-20" />
+          )}
+          <div className="py-3 flex flex-row">
+            <div className="flex flex-col pr-3">
+              <img
+                className="w-12 aspect-square"
+                src={experience.imgLogo}
+                alt="Logo empresa"
+              />
             </div>
-            <div className="pt-3">
-              {experience.positionsInCompany.map((position) => (
-                <div key={position.id}>
-                  <span className="relative block top-4 -left-9 h-2 w-2 rounded-full bg-[#0000004d]" />
-                  <div>
-                    <h1 className="text-base text-[#000000e6] leading-5 font-semibold">
-                      {position.title}
-                    </h1>
-                    <span className="text-base text-[#00000099] leading-5 font-normal">
-                      {formatDate(position.startDate)} -{" "}
-                      {formatDate(position.endDate)} ·{" "}
-                      {formatDateRange(position.startDate, position.endDate)}
-                    </span>
-                  </div>
-                  <div className="py-3 relative">
-                    <span
-                      className={`text-base text-[#000000e6] leading-5 font-normal ${
-                        modal ? "" : "line-clamp-2"
-                      }`}
-                      style={{ whiteSpace: "pre-line" }}
-                    >
-                      {position.description}
-                    </span>
-                    {!modal && (
-                      <button
-                        className="absolute right-0 bottom-[0.65rem] pl-2 bg-white"
-                        onClick={handleSeeMore}
+            <div className="w-full">
+              <div className="flex flex-col">
+                <span className="text-[#000006] text-base font-semibold leading-5">
+                  {experience.company}
+                </span>
+                <span className="text-[#000006] text-base font-normal leading-5">
+                  {experience.jobType} ·{" "}
+                  {formatDateRange(experience.startDate, experience.endDate)}
+                </span>
+                <span className="text-[#00000099] text-base font-normal leading-5">
+                  {formatTxtLocation(experience)}
+                </span>
+              </div>
+              <div className="pt-3">
+                {experience.positionsInCompany.map((position) => (
+                  <div key={position.id}>
+                    <span className="relative block top-4 -left-9 h-2 w-2 rounded-full bg-[#0000004d]" />
+                    <div>
+                      <h1 className="text-base text-[#000000e6] leading-5 font-semibold">
+                        {position.title}
+                      </h1>
+                      <span className="text-base text-[#00000099] leading-5 font-normal">
+                        {formatDate(position.startDate)} -{" "}
+                        {formatDate(position.endDate)} ·{" "}
+                        {formatDateRange(position.startDate, position.endDate)}
+                      </span>
+                    </div>
+                    <div className="py-3 relative">
+                      <span
+                        className={`text-base text-[#000000e6] leading-5 font-normal ${
+                          modal ? "" : "line-clamp-2"
+                        }`}
+                        style={{ whiteSpace: "pre-line" }}
                       >
-                        <span className="text-black text-opacity-60 text-base leading-5 font-normal">
-                          Ver mais ...
-                        </span>
-                      </button>
-                    )}
+                        {position.description}
+                      </span>
+                      {!modal && (
+                        <button
+                          className="absolute right-0 bottom-[0.65rem] pl-2 bg-white"
+                          onClick={handleSeeMore}
+                        >
+                          <span className="text-black text-opacity-60 text-base leading-5 font-normal">
+                            Ver mais ...
+                          </span>
+                        </button>
+                      )}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 }
