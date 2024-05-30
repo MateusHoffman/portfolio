a48f2138344067383227c206cc0bd0cf0f6207ed
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-29 23:46:35 -0300

    Updated img banner

--- src/data/local/ProfileData.ts
+++ src/data/local/ProfileData.ts
@@ -1,7 +1,7 @@
 export const profileData = {
   name: "Mateus Hoffman",
   picture: "https://media.licdn.com/dms/image/C4D03AQFz4U-QOZT7nQ/profile-displayphoto-shrink_200_200/0/1662759513105?e=1721260800&v=beta&t=zL2kHEAVJSa7sTVYjSqoEqoOyijzQkK9dnd1bofwRhg",
-  banner: "https://media.licdn.com/dms/image/D4D16AQHFJ8NjquWAxw/profile-displaybackgroundimage-shrink_350_1400/0/1667320641684?e=1721260800&v=beta&t=pf1t7cn-97Y17PsLCwsnRs_OGPcP5Tuy4EoaR3JX3pA",
+  banner: "/banner.png",
   description: "Desenvolvedor Mobile | React Native, TypeScript, Git, JavaScript",
   city: "Campinas",
   state: "São Paulo",
