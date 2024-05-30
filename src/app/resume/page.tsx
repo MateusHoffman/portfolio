34288dafdb49cff683665bd4e55800af6205b4ed
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 00:31:43 -0300

    Add button go back to mobile

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import { isMobile } from "react-device-detect";
 
 export default function Resume() {
   return (
     <div className="h-screen w-screen overflow-hidden bg-gray-800">
+      {isMobile && (
+        <Link href="/">
+          {/* Estilize o conteúdo dentro do Link */}
+          <div className="absolute h-10 w-10 p-1 pr-2 bg-primary-400 z-50 rounded-full flex justify-center items-center top-4 left-4">
+            <img src="/arrow.png" alt="Arrow" />
+          </div>
+        </Link>
+      )}
       <embed
         src="/mateus_hoffman_curriculo.pdf"
         type="application/pdf"
