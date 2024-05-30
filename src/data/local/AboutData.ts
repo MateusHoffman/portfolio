8253b17be70d25aa152f5f7d4672e63ab861170e
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 19:50:30 -0300

    Updated AboutData

--- src/data/local/AboutData.ts
+++ src/data/local/AboutData.ts
@@ -1,5 +1,11 @@
-export const aboutData = {
+interface AboutData {
+  mainText: string;
+  completeText: string;
+}
+
+export const aboutData: AboutData = {
   mainText:
     "Sou um desenvolvedor pleno especializado em Front-End mobile e web, com dois anos de experiência profissional. Tenho habilidades sólidas no trabalho em equipe, empregando soft skills para garantir uma colaboração eficaz. Minhas competências técnicas incluem desenvolvimento em JavaScript, TypeScript e Kotlin, utilizando os frameworks React, React Native e Jetpack Compose. Tenho um forte enfoque em responsividade, testes unitários e otimização de desempenho.",
-  completeText: "",
+  completeText:
+    "\n\n Ferramentas & Tecnologias \n\n Liguagens: X, Y e Z \n FrameWorks: A, B e C",
 };
