e6a977ed7f8ad5d020047f7169c8b930f5ece00b
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-29 21:56:54 -0300

    Created a function to download pdf

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -1,3 +1,34 @@
 export const delay = (ms: number) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export function downloadPDF(pdfUrl: string, pdfFileName: string): void {
+  fetch(pdfUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro ao baixar o PDF: " + response.status);
+      }
+      return response.blob();
+    })
+    .then((blob) => {
+      // Cria um objeto URL temporário para o blob
+      const url = window.URL.createObjectURL(blob);
+
+      // Cria um elemento <a> para iniciar o download
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", pdfFileName);
+
+      // Adiciona o elemento <a> ao DOM
+      document.body.appendChild(link);
+
+      // Simula um clique no link para iniciar o download
+      link.click();
+
+      // Remove o elemento <a> do DOM após o download
+      document.body.removeChild(link);
+    })
+    .catch((error) => {
+      console.error("Erro ao baixar o PDF:", error);
+    });
+}
