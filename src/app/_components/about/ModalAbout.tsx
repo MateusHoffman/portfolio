32c8f04a197264f1ac60f14d2fc3aa99b0b9b81f
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 11:49:19 -0300

    Implemented boldWordsInText in ModalAbout

--- src/app/_components/about/ModalAbout.tsx
+++ src/app/_components/about/ModalAbout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Modal from "@/components/modal/Modal";
 import { aboutData } from "@/data/local/AboutData";
+import { boldWordsInText } from "@/app/util/format";
 
 interface IProps {
   isOpen: boolean;
@@ -8,12 +9,32 @@ interface IProps {
 }
 
 export default function ModalAbout(props: IProps) {
+  const wordsToBold = [
+    "Front-End",
+    "mobile",
+    "web",
+    "dois anos",
+    "soft skills",
+    "JavaScript",
+    "TypeScript",
+    "Kotlin",
+    "React",
+    "Native",
+    "Jetpack Compose",
+    "responsividade",
+    "testes",
+    "desempenho",
+  ];
+
   return (
     <Modal isOpen={props.isOpen} setIsOpen={props.setIsOpen} title="Sobre">
       <div>
-        <p className="text-black text-opacity-90 text-base leading-5 font-normal">
-          {aboutData.mainText}
-        </p>
+        <p
+          className="py-3 text-black text-opacity-90 text-base leading-5 font-normal"
+          dangerouslySetInnerHTML={{
+            __html: boldWordsInText(aboutData.mainText, wordsToBold),
+          }}
+        />
       </div>
     </Modal>
   );
