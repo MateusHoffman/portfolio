4ad561f7871906cccd7ae8818efa63e8e511722f
Hoffman <mateus.hsilva@muu.agr.br>
2024-05-30 10:18:25 -0300

    Updated description ExperienceData

--- src/data/local/ExperienceData.ts
+++ src/data/local/ExperienceData.ts
@@ -1,5 +1,5 @@
-import moment from 'moment';
-import { v4 as uuidv4 } from 'uuid';
+import moment from "moment";
+import { v4 as uuidv4 } from "uuid";
 
 interface Position {
   id: string;
@@ -33,9 +33,10 @@ export const experienceData: ExperienceData = {
       id: uuidv4(),
       company: "MUU Agrotech",
       jobType: "Tempo integral",
-      imgLogo: "https://media.licdn.com/dms/image/D4E0BAQGDIMF0chtozQ/company-logo_100_100/0/1691426534167?e=1724284800&v=beta&t=-TI6N0Wd9-4i2LEjWokI0QFkXmZ_v5DMbq-rStkXLaY",
+      imgLogo:
+        "https://media.licdn.com/dms/image/D4E0BAQGDIMF0chtozQ/company-logo_100_100/0/1691426534167?e=1724284800&v=beta&t=-TI6N0Wd9-4i2LEjWokI0QFkXmZ_v5DMbq-rStkXLaY",
       startDate: "13/03/2023",
-      endDate: moment().format('DD/MM/YYYY'),
+      endDate: moment().format("DD/MM/YYYY"),
       city: "Campinas",
       state: "São Paulo",
       country: "Brasil",
@@ -45,17 +46,17 @@ export const experienceData: ExperienceData = {
           id: uuidv4(),
           title: "Desenvolvedor de aplicativos móveis Pleno",
           startDate: "13/07/2023",
-          endDate: moment().format('DD/MM/YYYY'),
-          description:
-            "Eu desenvolvo aplicativos de celular que são visualmente atraentes e altamente otimizados para proporcionar uma experiência incrível aos usuários.",
+          endDate: moment().format("DD/MM/YYYY"),
+          description: `Participei do desenvolvimento do aplicativo Muu Biometria, onde integrei uma câmera com inteligência artificial para reconhecimento biométrico e implementei um mapa para cadastrar pastos em uma fazenda. Também contribuí para o aplicativo Muu Cargo, implementando um mapa com rotas otimizadas para minimizar o tempo de viagem e reduzir o impacto nos animais.
+          \nHabilidades usadas: Git, TypeScript, React Native, Realm...`,
         },
         {
           id: uuidv4(),
           title: "Desenvolvedor Full-Stack Júnior",
           startDate: "13/03/2023",
           endDate: "13/07/2023",
-          description:
-            "Desenvolvo sites e aplicativos responsivos e atraentes, além de criar bancos de dados altamente organizados para garantir a eficiência e a usabilidade.",
+          description: `Desenvolvi projetos responsivos com foco em desempenho, seguindo o Figma.
+          \nHabilidades usadas: Git, JavaScript, TypeScript, React.js, Figma, RESTful APIs, Jest, ContextAPI, Node, Express, NoSQL...`,
         },
       ],
     },
@@ -63,9 +64,10 @@ export const experienceData: ExperienceData = {
       id: uuidv4(),
       company: "Freelancer",
       jobType: "Freelancer",
-      imgLogo: "https://media.licdn.com/dms/image/D4D0BAQFSEbmwaeNAUw/company-logo_100_100/0/1683928977056/liberoprofessionista_logo?e=1724284800&v=beta&t=B1k2uW3Cx-fYB5q15_7DFYLjnpNsWj8zV31BoHN9PVE",
+      imgLogo:
+        "https://media.licdn.com/dms/image/D4D0BAQFSEbmwaeNAUw/company-logo_100_100/0/1683928977056/liberoprofessionista_logo?e=1724284800&v=beta&t=B1k2uW3Cx-fYB5q15_7DFYLjnpNsWj8zV31BoHN9PVE",
       startDate: "01/02/2022",
-      endDate: moment().format('DD/MM/YYYY'),
+      endDate: moment().format("DD/MM/YYYY"),
       city: null,
       state: null,
       country: "Brasil",
@@ -75,9 +77,9 @@ export const experienceData: ExperienceData = {
           id: uuidv4(),
           title: "Desenvolvedor Web",
           startDate: "01/02/2022",
-          endDate: moment().format('DD/MM/YYYY'),
-          description:
-            "Realizei projetos para clientes atendendo cada necessidade e elaborando um fluxo de desenvolvimento usando as melhores tecnologias disponíveis no mercado e versionando todas aplicações com o Git.",
+          endDate: moment().format("DD/MM/YYYY"),
+          description: `Desenvolvi websites responsivos com excelente desempenho, utilizando práticas avançadas de front-end e focando na experiência do usuário. Além disso, implementei APIs RESTful robustas e desenvolvi algoritmos eficientes para processamento de dados.
+          \nHabilidades usadas: Git, JavaScript, TypeScript, React.js, Next.js, TaildWind, RESTful APIs, Jest, React Testing Library, Redux, ContextAPI, Node, Express, SQL, NoSQL, Docker, Docker compose, Dockerfile...`,
         },
       ],
     },
